4c24db82a6056db866f67e5700267f7986282d0f
kuothmaar <kuothmaar@gmail.com>
2025-12-05 15:08:25 +0300

    Add files via upload

---
+++ NoteBooks/started/python_101.py
@@ -0,0 +1,19 @@
+# this is the comment -python ignores this completely
+#use comments to explanin what your code does
+print("hello,world")# this actually does comethings
+
+
+
+#avariables
+name="sarah"  #name  is the variable  name  &  sarah is the value
+course="data Analysis"
+age=20
+city="kakuma"
+gender="females"
+is_student=True
+
+#printing avriables
+print(name) #this prints the value of the variable  name
+
+name="Georage"# variables can be reassigned
+#printing  variable 
---
+++ NoteBooks/started/strings.py
@@ -0,0 +1,14 @@
+#striing methods
+# 1 upper and lower
+text = "Hello World"
+#convert to upper case
+print(text.upper())
+#convert to lower case
+text = "Hello World"
+#convert to lower case
+
+text="my name is edith and i love dogs"
+
+#convert to title 
+
+
